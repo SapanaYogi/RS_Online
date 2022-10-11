30b171d87dd14ad0ce06d2468dd6814a33635f62
HP <HP@LAPTOP-JFU89ISH>
2022-10-11 15:18:59 +0530

    My first Git Program

---
+++ src/samplepack/logical1.java
@@ -0,0 +1,15 @@
+package samplepack;
+
+import java.util.Scanner;
+
+public class logical1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter string");
+		String a=scan.next();
+		System.out.println(a);
+	}
+
+}
